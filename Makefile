1c66c500cbbc5010d1bebad7d83a8c1d211ae275
Warren Craddock <warren.craddock@gmail.com>
2014-10-19 19:41:11 -0700

    Removed dependency on assimp.

--- Makefile
+++ Makefile
@@ -1,8 +1,2 @@
 wookie_glfw : wookie_glfw.cc common/controls.cpp
-	g++ -o wookie_glfw -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -lglfw3 -lassimp -I. wookie_glfw.cc common/controls.cpp
-
-simple_sample : simple_sample.cc
-	g++ -o simple_sample -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -lglfw3 -lassimp -I. simple_sample.cc
-
-simple_obj : simple_obj.cc
-	g++ -o simple_obj -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -lglfw3 -lassimp -I. simple_obj.cc
+	g++ -o wookie_glfw -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -lglfw3 -I. wookie_glfw.cc common/controls.cpp
--- wookie_glfw.cc
+++ wookie_glfw.cc
@@ -11,10 +11,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-#include <assimp/cimport.h>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -33,9 +29,7 @@ static float angle = 0.f;
 #define aisgl_max(x, y) (y > x ? y : x)
 
 /* the global Assimp scene object */
-const aiScene *scene = NULL;
 GLuint human_display_list = 0;
-aiVector3D scene_min, scene_max, scene_center;
 
 // Models for the human body and for the jacket.
 Model_OBJ human_body_obj;
@@ -57,228 +51,6 @@ void reshape(int width, int height) {
   glLoadIdentity();
   gluPerspective(fieldOfView, aspectRatio, 1.0, 1000.0); /* Znear and Zfar */
   glViewport(0, 0, width, height);
-}
-
-/* ----------------------------------------------------------------------------
- */
-void get_bounding_box_for_node(const aiNode *nd, aiVector3D *min,
-                               aiVector3D *max, aiMatrix4x4 *trafo) {
-  aiMatrix4x4 prev;
-  unsigned int n = 0, t;
-
-  prev = *trafo;
-  aiMultiplyMatrix4(trafo, &nd->mTransformation);
-
-  for (; n < nd->mNumMeshes; ++n) {
-    const aiMesh *mesh = scene->mMeshes[nd->mMeshes[n]];
-    for (t = 0; t < mesh->mNumVertices; ++t) {
-
-      aiVector3D tmp = mesh->mVertices[t];
-      aiTransformVecByMatrix4(&tmp, trafo);
-
-      min->x = aisgl_min(min->x, tmp.x);
-      min->y = aisgl_min(min->y, tmp.y);
-      min->z = aisgl_min(min->z, tmp.z);
-
-      max->x = aisgl_max(max->x, tmp.x);
-      max->y = aisgl_max(max->y, tmp.y);
-      max->z = aisgl_max(max->z, tmp.z);
-    }
-  }
-
-  for (n = 0; n < nd->mNumChildren; ++n) {
-    get_bounding_box_for_node(nd->mChildren[n], min, max, trafo);
-  }
-  *trafo = prev;
-}
-
-/* ----------------------------------------------------------------------------
- */
-void get_bounding_box(aiVector3D *min, aiVector3D *max) {
-  aiMatrix4x4 trafo;
-  aiIdentityMatrix4(&trafo);
-
-  min->x = min->y = min->z = 1e10f;
-  max->x = max->y = max->z = -1e10f;
-  get_bounding_box_for_node(scene->mRootNode, min, max, &trafo);
-}
-
-/* ----------------------------------------------------------------------------
- */
-void color4_to_float4(const aiColor4D *c, float f[4]) {
-  f[0] = c->r;
-  f[1] = c->g;
-  f[2] = c->b;
-  f[3] = c->a;
-}
-
-/* ----------------------------------------------------------------------------
- */
-void set_float4(float f[4], float a, float b, float c, float d) {
-  f[0] = a;
-  f[1] = b;
-  f[2] = c;
-  f[3] = d;
-}
-
-/* ----------------------------------------------------------------------------
- */
-void apply_material(const aiMaterial *mtl) {
-  float c[4];
-
-  GLenum fill_mode;
-  int ret1, ret2;
-  aiColor4D diffuse;
-  aiColor4D specular;
-  aiColor4D ambient;
-  aiColor4D emission;
-  float shininess, strength;
-  int two_sided;
-  int wireframe;
-  unsigned int max;
-
-  set_float4(c, 0.8f, 0.8f, 0.8f, 1.0f);
-  if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
-    color4_to_float4(&diffuse, c);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, c);
-
-  set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
-  if (AI_SUCCESS ==
-      aiGetMaterialColor(mtl, AI_MATKEY_COLOR_SPECULAR, &specular))
-    color4_to_float4(&specular, c);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
-
-  set_float4(c, 0.2f, 0.2f, 0.2f, 1.0f);
-  if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_AMBIENT, &ambient))
-    color4_to_float4(&ambient, c);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, c);
-
-  set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
-  if (AI_SUCCESS ==
-      aiGetMaterialColor(mtl, AI_MATKEY_COLOR_EMISSIVE, &emission))
-    color4_to_float4(&emission, c);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, c);
-
-  max = 1;
-  ret1 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS, &shininess, &max);
-  if (ret1 == AI_SUCCESS) {
-    max = 1;
-    ret2 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS_STRENGTH, &strength,
-                                   &max);
-    if (ret2 == AI_SUCCESS)
-      glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess * strength);
-    else
-      glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
-  } else {
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 0.0f);
-    set_float4(c, 0.0f, 0.0f, 0.0f, 0.0f);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
-  }
-
-  max = 1;
-  if (AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_ENABLE_WIREFRAME,
-                                              &wireframe, &max))
-    fill_mode = wireframe ? GL_LINE : GL_FILL;
-  else
-    fill_mode = GL_FILL;
-  glPolygonMode(GL_FRONT_AND_BACK, fill_mode);
-
-  max = 1;
-  if ((AI_SUCCESS ==
-       aiGetMaterialIntegerArray(mtl, AI_MATKEY_TWOSIDED, &two_sided, &max)) &&
-      two_sided)
-    glDisable(GL_CULL_FACE);
-  else
-    glEnable(GL_CULL_FACE);
-}
-
-/* ----------------------------------------------------------------------------
- */
-void recursive_render(const aiScene *sc, const aiNode *nd) {
-  printf("Top of recursive_render...\n");
-  unsigned int i;
-  unsigned int n = 0, t;
-  aiMatrix4x4 m = nd->mTransformation;
-
-  /* update transform */
-  aiTransposeMatrix4(&m);
-  glPushMatrix();
-  glMultMatrixf((float *)&m);
-
-  /* draw all meshes assigned to this node */
-  for (; n < nd->mNumMeshes; ++n) {
-    const aiMesh *mesh = scene->mMeshes[nd->mMeshes[n]];
-
-    apply_material(sc->mMaterials[mesh->mMaterialIndex]);
-
-    if (mesh->mNormals == NULL) {
-      glDisable(GL_LIGHTING);
-    } else {
-      glEnable(GL_LIGHTING);
-    }
-
-    for (t = 0; t < mesh->mNumFaces; ++t) {
-      const aiFace *face = &mesh->mFaces[t];
-      GLenum face_mode;
-
-      switch (face->mNumIndices) {
-      case 1:
-        face_mode = GL_POINTS;
-        break;
-      case 2:
-        face_mode = GL_LINES;
-        break;
-      case 3:
-        face_mode = GL_TRIANGLES;
-        break;
-      default:
-        face_mode = GL_POLYGON;
-        break;
-      }
-
-      glBegin(face_mode);
-
-      for (i = 0; i < face->mNumIndices; i++) {
-        int index = face->mIndices[i];
-        if (mesh->mColors[0] != NULL)
-          glColor4fv((GLfloat *)&mesh->mColors[0][index]);
-        if (mesh->mNormals != NULL)
-          glNormal3fv(&mesh->mNormals[index].x);
-        glVertex3fv(&mesh->mVertices[index].x);
-      }
-
-      glEnd();
-    }
-  }
-
-  /* draw all children */
-  for (n = 0; n < nd->mNumChildren; ++n) {
-    recursive_render(sc, nd->mChildren[n]);
-  }
-
-  glPopMatrix();
-}
-
-/* ----------------------------------------------------------------------------
- */
-void do_motion(void) {
-  static GLint prev_time = 0;
-  static GLint prev_fps_time = 0;
-  static int frames = 0;
-
-  // TODO(wcraddock): make sure this still works in glfw
-  int time = glfwGetTime();
-  angle += (time - prev_time) * 0.01;
-  prev_time = time;
-
-  frames += 1;
-  if ((time - prev_fps_time) > 1000) /* update every seconds */
-  {
-    int current_fps = frames * 1000 / (time - prev_fps_time);
-    printf("%d fps\n", current_fps);
-    frames = 0;
-    prev_fps_time = time;
-  }
 }
 
 #define TOTAL_FLOATS_IN_TRIANGLE 9
@@ -428,9 +200,6 @@ void draw_hairs() {
 
 void display(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // Center the model.
-  glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);
 
   // Create a display list for the human body and jacket objects,
   // which never change.
