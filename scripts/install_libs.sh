1c4cacd1f9d32b54cb3a242721b1337828c1e267
Girts <girts.folkmanis@gmail.com>
2014-12-07 16:35:20 -0800

    Fail if any of the install_libs.sh parts fails.

--- scripts/install_libs.sh
+++ scripts/install_libs.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+set -o errexit
+
 # Get the location of this script.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HALLUCINATION_DIR="${DIR}/.."
 
 # Build and install GLFW
-cd ${HALLUCINATION_DIR}/third_party/glfw-3.0.4 && \
-cmake -G 'Unix Makefiles' . && \
-make -j 32 && \
+cd ${HALLUCINATION_DIR}/third_party/glfw-3.0.4
+cmake -G 'Unix Makefiles' .
+make -j 32
 sudo make install
 
 # Build and install PortAudio
-cd ${HALLUCINATION_DIR}/third_party/portaudio && \
-./configure && \
-make -j 32 && \
+cd ${HALLUCINATION_DIR}/third_party/portaudio
+./configure
+make -j 32
 sudo make install
 
 # Build and install aubio
-cd ${HALLUCINATION_DIR}/third_party/aubio && \
-./waf configure && \
-./waf build && \
+cd ${HALLUCINATION_DIR}/third_party/aubio
+./waf configure
+./waf build
 sudo ./waf install
